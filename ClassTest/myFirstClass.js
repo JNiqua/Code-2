e3a0015ef351397d9cc51c6de2b66f017372aa04
Jannick Klose <ja.l.klose@gmail.com>
2024-10-23 15:02:03 +0200

    my first class *cough cough*

---
+++ ClassTest/myFirstClass.js
@@ -0,0 +1,26 @@
+"use strict";
+var MyfirstClass;
+(function (MyfirstClass) {
+    class Vector {
+        constructor(_x, _y) {
+            this.set(_x, _y);
+        }
+        scale(_factor) {
+            this.x *= _factor;
+            this.y *= _factor;
+        }
+        add(_addend) {
+            this.x += _addend.x;
+            this.y += _addend.y;
+        }
+        set(_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+    }
+    let v1 = new Vector(5, 4);
+    //v1.set(1, 4);
+    v1.scale(2);
+    console.log(v1);
+})(MyfirstClass || (MyfirstClass = {}));
+//# sourceMappingURL=myFirstClass.js.map
